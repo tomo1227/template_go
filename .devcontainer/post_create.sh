ee33193d86d6c4cb6b77cb5a1624156dacba67d4
tomo1227 <tomo.soccer1227@gmail.com>
2024-03-03 08:52:24 +0000

    feat👍 不要なものを削除

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -6,20 +6,6 @@ echo "source ~/.git-completion.bash" >> ~/.bashrc
 # bash auto completion
 sudo apt update && sudo apt install bash-completion
 
-echo "pip Installing"
-pip install --upgrade aws-sam-cli
-sam --version
-
-# localstack
-# see: https://qiita.com/Brutus/items/89eb03be7c7bd9d6911d
-echo "localstack Installing"
-pip install localstack
-localstack --version
-
-echo "awscli-local Installing"
-pip install awscli-local
-awslocal --version
-
 echo "air Installing"
 go install github.com/cosmtrek/air@latest
 
