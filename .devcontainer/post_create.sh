bd719e86bdf8080afa446eb65032cebbb74b03da
tomo1227 <tomo.soccer1227@gmail.com>
2024-03-06 17:28:12 +0000

    feat👍 templateのciの修正

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -6,9 +6,6 @@ echo "source ~/.git-completion.bash" >> ~/.bashrc
 # bash auto completion
 sudo apt update && sudo apt install bash-completion
 
-echo "air Installing"
-go install github.com/cosmtrek/air@latest
-
 go mod tidy
 cd cmd/api
 go mod tidy
