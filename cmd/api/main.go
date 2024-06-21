fa45798b86696ed34f1717143d56bf0519770f38
Tomoki Ota <tomo.soccer1227@gmail.com>
2024-06-22 08:31:41 +0900

    feat🎉 badgeの追加 (#25)
    * ✨feat  badgeの追加
    
    * 📚fix: Readmeの修正
    
    * 📚fix readmeの修正

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,22 @@
+// mainパッケージは、Fiberフレームワークを使用してシンプルなHTTPサーバーを作成します。
 package main
 
 import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// main関数は、HTTPサーバーを起動し、"/health"エンドポイントを設定します。
 func main() {
 	app := fiber.New()
 
+	// GETリクエストに応答するための"/health"エンドポイントを設定します。
+	// レスポンスとしてJSON形式でステータス情報を返します。
 	app.Get("/health", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
 			"status": "OK!",
 		})
 	})
+
+	// サーバーをポート8080でリッスンします。
 	app.Listen(":8080")
 }
